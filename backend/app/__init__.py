2f10ad59bd0c89cfe878138543a59443c73ca666
Esteban valladares <paindksn2210valladares@gmail.com>
2026-09-01 10:40:58 -0600

    actualizacion de datos

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,11 +5,10 @@ import os
 def create_app():
     app = Flask(__name__)
     
-    # Le decimos a Flask que se conecte a PostgreSQL usando las variables de Docker
     db_user = os.environ.get('POSTGRES_USER', 'postgres')
     db_pass = os.environ.get('POSTGRES_PASSWORD', 'bellidiel123')
     db_name = os.environ.get('POSTGRES_DB', 'bellidiel_db')
-    db_host = os.environ.get('DB_HOST', 'db') # 'db' es el nombre del contenedor en Docker
+    db_host = os.environ.get('DB_HOST', 'db')
     
     app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_pass}@{db_host}:5432/{db_name}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -18,7 +17,7 @@ def create_app():
 
     with app.app_context():
         from . import models
-        db.create_all() # Crea las tablas 
+        db.create_all()
 
     # Ruta de prueba 
     @app.route('/status', methods=['GET'])
